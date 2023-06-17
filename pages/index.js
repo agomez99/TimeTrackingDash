a7ce22993cc252a7ed2bc4045a33ac60a3e18436
Austine Gomez <austine.gomez@yahoo.com>
2023-06-17 17:26:22 -0500

    dots work

--- pages/index.js
+++ pages/index.js
@@ -9,12 +9,13 @@ export default function Home() {
   const [selectedTimeframe, setSelectedTimeframe] = useState('daily');
 
   const timeframes = ['daily', 'weekly', 'monthly'];
+  const dots = Array(3).fill('.'); // creates an array with 3 dots
+
+
 
   const handleTimeframeClick = (timeframe) => {
     setSelectedTimeframe(timeframe);
   }
-
-
 
   const { title: workTitle, timeframes: workTimeframes } = data[0];
   const { title: playTitle, timeframes: playTimeframes } = data[1];
@@ -23,11 +24,11 @@ export default function Home() {
   const { title: socialTitle, timeframes: socialTimeframes } = data[4];
   const { title: selfCareTitle, timeframes: selfCareTimeframes } = data[5];
 
-
   const displayData = (data, title) => {
     const timeframeData = data[selectedTimeframe];
     const currentValue = timeframeData ? timeframeData.current : null;
     const previousValue = timeframeData ? timeframeData.previous : null;
+
 
     return (
       <div >
@@ -37,15 +38,36 @@ export default function Home() {
         </Head>
         <Card className={`${styles.card1} ${title.toLowerCase()}`}>
           <Card.Body>
-            <h5>{title}</h5>
-            <p className={styles.card_value}>{currentValue}hrs</p>
-            <p>Last Week - {previousValue}hrs</p>
+            <div className={styles.cardtopinfo}>
+              <h5>{title}</h5>
+              <h5 className={styles.card_dots}>
+              {dots.map((dot, index) => (
+                <a
+                  key={index}
+                  onClick={() => handleTimeframeClick(timeframes[index])}
+                  className={timeframes[index] === selectedTimeframe ? styles.selected : ''}
+                  style={{
+                    textDecoration: 'none',
+                    color: 'hsl(235, 45%, 61%)',
+                    fontSize: '1.5rem',
+                    fontWeight: 'bold'
+                  }}
+                >
+                  {dot}
+                </a>
+              ))}
+            </h5>
+            </div>
+            <div className={styles.cardbottominfo}>
+              <p className={styles.card_value}>{currentValue}hrs</p>
+              <p className={styles.card_previous}>Last Week - {previousValue}hrs</p>
+            </div>
           </Card.Body>
         </Card>
       </div>
     );
   }
-
+  
   return (
     <div className={styles.container}>
       <Head>
@@ -55,11 +77,12 @@ export default function Home() {
         <Container fluid>
           <Row>
             <Col>
-
               <div className={styles.card_avatar}>
-                <Image src="/images/image-jeremy.png" alt="avatar" width={100} height={100} />
-                <p>Report for</p>
-                <h1 className={styles.card_title}>Jeremy Robson.</h1>
+                <Image src="/images/image-jeremy.png" alt="avatar" className={styles.avatar} width={100} height={100} />
+                <div className={styles.card_avatar_info}>
+                  <p className={styles.report_for}>Report for</p>
+                  <h1 className={styles.card_title}>Jeremy Robson.</h1>
+                </div>
               </div>
               <Card className={styles.card_main}>
                 <Card.Body>
@@ -70,6 +93,13 @@ export default function Home() {
                           id={timefr}
                           onClick={() => handleTimeframeClick(timefr)}
                           className={timefr === selectedTimeframe ? styles.selected : ''}
+                          style={{
+                            textDecoration: 'none',
+                            color: 'hsl(235, 45%, 61%)',
+                            fontSize: '1.2rem',
+                            fontWeight: 'bold',
+                            fontStyle: "'Outfit', sans-serif'",
+                          }}
                         >
                           {timefr}
                         </a>
